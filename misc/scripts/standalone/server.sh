3a527484c09fdc7efc998415a2b8d23fec6e2728
lqh2307 <168527505+lqh2307@users.noreply.github.com>
2024-06-10 08:52:33 +0700

    Init

--- misc/scripts/standalone/server.sh
+++ misc/scripts/standalone/server.sh
@@ -1,31 +1,13 @@
 #!/bin/sh
 
-cd "$(dirname "$0")"
-# BRouter standalone server
-# java -cp brouter.jar btools.brouter.RouteServer <segmentdir> <profile-map> <customprofiledir> <port> <maxthreads> [bindaddress]
+JAVA_OPTS="-Xmx128M -Xms128M -Xmn8M"
+ 
+export SEGMENTSPATH=/segments4
+export PROFILESPATH=/profiles2
+export CUSTOMPROFILESPATH=/customprofiles
 
-# maxRunningTime is the request timeout in seconds, set to 0 to disable timeout
-JAVA_OPTS="-Xmx128M -Xms128M -Xmn8M -DmaxRunningTime=300"
-
-# If paths are unset, first search in locations matching the directory structure
-# as found in the official BRouter zip archive
-CLASSPATH=${CLASSPATH:-"../brouter.jar"}
-SEGMENTSPATH=${SEGMENTSPATH:-"../segments4"}
-PROFILESPATH=${PROFILESPATH:-"../profiles2"}
-CUSTOMPROFILESPATH=${CUSTOMPROFILESPATH:-"../customprofiles"}
-
-# Otherwise try to locate files inside the source checkout
-if [ ! -e "$CLASSPATH" ]; then
-    CLASSPATH="$(ls ../../../brouter-server/build/libs/brouter-*-all.jar | sort --reverse --version-sort | head --lines 1)"
-fi
-if [ ! -e "$SEGMENTSPATH" ]; then
-    SEGMENTSPATH="../../segments4"
-fi
-if [ ! -e "$PROFILESPATH" ]; then
-    PROFILESPATH="../../profiles2"
-fi
-if [ ! -e "$CUSTOMPROFILESPATH" ]; then
-    CUSTOMPROFILESPATH="../customprofiles"
+if [ -n "${OSM_PBF_FILE}" ]; then
+  /bin/process_osm_pbf.sh
 fi
 
-java $JAVA_OPTS -cp $CLASSPATH btools.server.RouteServer "$SEGMENTSPATH" "$PROFILESPATH" "$CUSTOMPROFILESPATH" 17777 1 $BINDADDRESS
+java ${JAVA_OPTS} -cp /brouter.jar btools.server.RouteServer ${SEGMENTSPATH} ${PROFILESPATH} ${CUSTOMPROFILESPATH} 17777 1
